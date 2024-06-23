6ad48b10d391b1ab8c99895e26420e50c5b911f7
Glitch 9 Inc <munchkin@glitch9.dev>
2024-06-23 16:11:02 +0900

    Version 0.10.63 Build 1063 Date 2024-06-23

--- Editor/Internal/InternalGUI/InternalGUI.cs
+++ Editor/Internal/InternalGUI/InternalGUI.cs
@@ -4,6 +4,14 @@ using MessageType = UnityEditor.MessageType;
 
 namespace Glitch9.ExtendedEditor
 {
+    public enum EditorStatus
+    {
+        Unset,
+        Okay,
+        Error,
+        Warning,
+    }
+    
     public class InternalGUI
     {
         private const float BIG_BTN_HEIGHT = 30;
@@ -32,6 +40,44 @@ namespace Glitch9.ExtendedEditor
             EditorGUILayout.Space();
             EditorGUILayout.LabelField(label, EGUIStyles.title);
             EGUIUtility.DrawTitleLine();
+        }
+
+        public static void StatusBox(string message, EditorStatus status)
+        {
+            Texture statusIcon;
+
+            switch (status)
+            {
+                case EditorStatus.Okay:
+                    statusIcon = EditorIcons.StatusCheck;
+                    break;
+                case EditorStatus.Error:
+                    statusIcon = EditorIcons.StatusError;
+                    break;
+                case EditorStatus.Warning:
+                    statusIcon = EditorIcons.StatusWarning;
+                    break;
+                default:
+                    statusIcon = null;
+                    break;
+            }
+
+            GUILayout.BeginHorizontal(EditorStyles.helpBox);
+            {
+                if (statusIcon != null)
+                {
+                    GUILayout.Label(statusIcon, GUILayout.Width(30), GUILayout.Height(30));
+                }
+
+                GUILayout.BeginVertical(); // Begin a vertical layout for centering
+                {
+                    GUILayout.FlexibleSpace(); // Add flexible space at the top
+                    GUILayout.Label(message, EditorStyles.wordWrappedLabel);
+                    GUILayout.FlexibleSpace(); // Add flexible space at the bottom
+                }
+                GUILayout.EndVertical(); // End the vertical layout
+            }
+            GUILayout.EndHorizontal();
         }
 
         public static void ScriptableObjectNotInResourcesFolder<TScriptableObject>() where TScriptableObject : ScriptableObject
