e0508269babd96866ccb6fbf3f8beef5522583c7
Glitch 9 Inc <munchkin@glitch9.dev>
2024-06-28 18:35:59 +0900

    Version 0.10.62 Build 1062 Date 2024-06-28

--- System/ScriptableObjects/ScriptableResource.cs
+++ System/ScriptableObjects/ScriptableResource.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using UnityEngine;
 
 namespace Glitch9.ScriptableObjects
@@ -6,21 +5,8 @@ namespace Glitch9.ScriptableObjects
     public abstract class ScriptableResource<TSelf> : ScriptableObject
         where TSelf : ScriptableResource<TSelf>
     {
-        protected static string AssetPath => $"Resources/{typeof(TSelf).Name}";
 
         private static TSelf _instance;
         public static TSelf Instance => _instance == null ? _instance = AssetUtils.LoadResource<TSelf>(true) : _instance;
-
-        public static TSelf Create(TSelf obj)
-        {
-#if UNITY_EDITOR
-            string path = AssetPath + ".asset";
-            string dir = Path.GetDirectoryName(path);
-            if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir)) Directory.CreateDirectory(dir);
-            UnityEditor.AssetDatabase.CreateAsset(obj, path);
-            UnityEditor.EditorUtility.SetDirty(obj);
-#endif
-            return obj;
-        }
     }
 }
